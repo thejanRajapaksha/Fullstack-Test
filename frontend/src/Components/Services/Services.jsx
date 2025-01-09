07b61c7126a0e766055b1bb37e568dea9459a44d
thejanRajapaksha <rmhthejan31@gmail.com>
2025-01-09 20:25:36 +0530

    Update Services.jsx

--- frontend/src/Components/Services/Services.jsx
+++ frontend/src/Components/Services/Services.jsx
@@ -13,25 +13,25 @@ export default class Services extends Component {
         icon: <FaCocktail />,
         title: "free cocktails",
         info:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores est eaque error provident unde eligendi.",
+          "Enjoy a variety of delicious, expertly crafted cocktails on the house. Whether you prefer a classic martini, a refreshing mojito, or something more adventurous, we've got the perfect drink waiting for you!",
       },
       {
         icon: <FaHiking />,
         title: "endless hiking",
         info:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores est eaque error provident unde eligendi.",
+          "Explore miles of stunning trails with breathtaking views. From serene forest walks to challenging mountain hikes, our endless hiking opportunities cater to all skill levels. Embrace nature and discover hidden gems along the way",
       },
       {
         icon: <FaShuttleVan />,
         title: "free shuttle",
         info:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores est eaque error provident unde eligendi.",
+          "Forget the hassle of transportation. Our complimentary shuttle service ensures you can easily explore the area, whether you're heading to the city center, nearby attractions, or just relaxing at your destination",
       },
       {
         icon: <FaBeer />,
         title: "storages beer",
         info:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores est eaque error provident unde eligendi.",
+          "Quench your thirst with an ice-cold beer anytime. We offer a variety of locally brewed and international beers, stored at the perfect temperature to keep them crisp and refreshing throughout your stay",
       },
     ],
   };
